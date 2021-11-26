7d19d9d498cde6de47b1f58bc248bfa0d688cc55
djsilva <coxcorp@gmail.com>
2021-11-26 17:36:51 -0300

    atualização table.jsx

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,39 +3,49 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 class Table extends React.Component {
-  render() {
+  renderTableHeader() {
+    return (
+      <tr>
+        <th> Descrição </th>
+        <th> Tag </th>
+        <th> Método de pagamento </th>
+        <th> Valor </th>
+        <th> Moeda </th>
+        <th> Câmbio utilizado </th>
+        <th> Valor convertido </th>
+        <th> Moeda de conversão </th>
+        <th> Editar/Excluir </th>
+      </tr>
+    );
+  }
+
+  renderTable() {
     const { getExpenses } = this.props;
     return (
-      <tbody>
-        <tr>
-          <th> Descrição </th>
-          <th> Tag </th>
-          <th> Método de pagamento </th>
-          <th> Valor </th>
-          <th> Moeda </th>
-          <th> Câmbio utilizado </th>
-          <th> Valor convertido </th>
-          <th> Moeda de conversão </th>
-          <th> Editar/Excluir </th>
+      getExpenses.map((item) => (
+        <tr key={ item.id }>
+          <td>{ item.description }</td>
+          <td>{ item.method }</td>
+          <td>{ item.tag }</td>
+          <td>{ item.value }</td>
+          <td>{ item.exchangeRates[item.currency].name.split('/')[0] }</td>
+          <td>{ Number(item.exchangeRates[item.currency].ask).toFixed(2) }</td>
+          <td>
+            {
+              Number(item.exchangeRates[item.currency].ask * item.value).toFixed(2)
+            }
+          </td>
+          <td>Real</td>
         </tr>
-        {
-          getExpenses.map((item) => (
-            <tr key={ item.id }>
-              <td>{ item.description }</td>
-              <td>{ item.method }</td>
-              <td>{ item.tag }</td>
-              <td>{ item.value }</td>
-              <td>{ item.exchangeRates[item.currency].name.split('/')[0] }</td>
-              <td>{ Number(item.exchangeRates[item.currency].ask).toFixed(2) }</td>
-              <td>
-                {
-                  Number(item.exchangeRates[item.currency].ask * item.value).toFixed(2)
-                }
-              </td>
-              <td>Real</td>
-            </tr>
-          ))
-        }
+      ))
+    );
+  }
+
+  render() {
+    return (
+      <tbody>
+        { this.renderTableHeader() }
+        { this.renderTable() }
       </tbody>
     );
   }
